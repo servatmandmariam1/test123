cc4d9fbfc3f82e538f5836f9d297ae123ad49b83
Mariam <bellamy1313@gmail.com>
2019-11-22 19:51:00 +0200

    Updates

---
+++ precache-manifest.395b5533f8f2a1226cdc07b7e93d08c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af994d6f75fc90bf465c29f4f28015b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9a4e1ef7ded36472956",
+    "url": "./static/css/main.aa1a1c6d.chunk.css"
+  },
+  {
+    "revision": "a2a9aa1c631d97123e5d",
+    "url": "./static/js/2.d4f9c037.chunk.js"
+  },
+  {
+    "revision": "e9a4e1ef7ded36472956",
+    "url": "./static/js/main.d8f784b4.chunk.js"
+  },
+  {
+    "revision": "91ea0491fc05bcd1c89f",
+    "url": "./static/js/runtime-main.fc01850d.js"
+  }
+]);
